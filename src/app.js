86cc2e23626f9580f455a4e99920e73e5665c88b
Bruno de Rezende <brunoderezende@hotmail.com>
2017-11-06 09:18:03 -0200

    html5

--- src/app.js
+++ src/app.js
@@ -27,11 +27,4 @@ app.use('/', routes);
 if( process.env.NODE_ENV !== 'production' )
   reload(app);
 
-// Catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 export default app;
