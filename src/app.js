6c51beb2d2df4187d61330426fc15ba791bc5226
Bruno de Rezende <brunoderezende@hotmail.com>
2017-11-06 17:13:59 -0200

    compression

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ app.use(logger('dev', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.use('/scripts', express.static(path.join(__dirname, '..', 'node_modules')));
 
